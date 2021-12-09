738f6d714acd878634f2038c1377fea6f63135fa
Nguyễn Văn Khoa <95401921+nkoxduahau@users.noreply.github.com>
2021-12-09 17:20:59 +0700

    bài 1 của e nv4

---
+++ 006.cpp
@@ -0,0 +1,24 @@
+#include<stdio.h>
+int main()
+{
+  int n;
+  float S;
+  S = 0;
+  do
+  {
+    printf("\nNhap vao so n: ");
+    scanf("%d", &n);
+    if(n < 1)
+    {
+      printf("\nSo n phai lon hon hoac bang 1, vui long nhap lai !");
+    }
+  }while(n < 1);
+  for(int i = 1; i <= n; i++){
+    S = S + 1.0 / i;
+  }
+  
+  printf("\nTong 1 + 1/2 + ... + 1/%d la: %.2f",n, S);
+ 
+  printf("\n-----------------------------------------------------\n");
+}
+  
